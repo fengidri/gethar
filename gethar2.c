ee32c7546f43c92d20c0f077099f806226e9d277
fengidri <fengidri@gmail.com>
2015-06-07 23:25:14 +0800

    add js run. make console.log output to stdout.

--- gethar2.c
+++ gethar2.c
@@ -9,6 +9,8 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <webkit2/webkit2.h>
+#include <JavaScriptCore/JSStringRef.h>
+#include <JavaScriptCore/JSValueRef.h>
 void get_snapshot_finish(GObject *object,
                                      GAsyncResult *result,
                                      gpointer data)
@@ -22,6 +24,45 @@ void get_snapshot_finish(GObject *object,
 	cairo_surface_write_to_png(surface, "/tmp/webkitgtk_test.png");
     printf("3get_snapshot_finish\n");
 
+}
+static void web_view_javascript_finished(GObject      *object,
+                              GAsyncResult *result,
+                              gpointer      user_data)
+{
+    WebKitJavascriptResult *js_result;
+    JSValueRef              value;
+    JSGlobalContextRef      context;
+    GError                 *error = NULL;
+
+    js_result = webkit_web_view_run_javascript_finish(WEBKIT_WEB_VIEW(object), result, &error);
+    if (!js_result) {
+        g_warning ("Error running javascript: %s", error->message);
+        g_error_free (error);
+        return;
+    }
+    printf("###########\n");
+
+    context = webkit_javascript_result_get_global_context(js_result);
+    value = webkit_javascript_result_get_value(js_result);
+    if (JSValueIsString(context, value)) {
+        JSStringRef js_str_value;
+        gchar      *str_value;
+        gsize       str_length;
+    printf("###########\n");
+
+        js_str_value = JSValueToStringCopy(context, value, NULL);
+    printf("2###########\n");
+        str_length = JSStringGetMaximumUTF8CStringSize(js_str_value);
+    printf("3###########\n");
+        str_value = (gchar *)g_malloc(str_length);
+        JSStringGetUTF8CString(js_str_value, str_value, str_length);
+        JSStringRelease(js_str_value);
+        g_print("$$$$$$$$$$$$$$$4Script result: %s\n", str_value);
+        g_free (str_value);
+    } else {
+        g_warning("Error running javascript: unexpected return value");
+    }
+    webkit_javascript_result_unref(js_result);
 }
 static void web_view_load_changed(WebKitWebView  *web_view,
                                    WebKitLoadEvent load_event,
@@ -54,20 +95,25 @@ static void web_view_load_changed(WebKitWebView  *web_view,
                 get_snapshot_finish,
                 web_view
                 );
+        const gchar *script = "JSON.stringify(window.performance.timing)";
+        script = "console.log('@#@#@#----console-')";
+        webkit_web_view_run_javascript(web_view, script, NULL, web_view_javascript_finished, NULL);
         break;
     }
 }
 
 void ready_to_show(WebKitWebView *webView)
 {
-    printf("ready_to_show\n");
-    webkit_web_view_get_snapshot(webView,
-            WEBKIT_SNAPSHOT_REGION_FULL_DOCUMENT,
-            WEBKIT_SNAPSHOT_OPTIONS_NONE,
-            NULL,
-            get_snapshot_finish,
-            webView
-            );
+    printf("---------------ready-to-show\n");
+    //webkit_web_view_get_snapshot(webView,
+    //        WEBKIT_SNAPSHOT_REGION_FULL_DOCUMENT,
+    //        WEBKIT_SNAPSHOT_OPTIONS_NONE,
+    //        NULL,
+    //        get_snapshot_finish,
+    //        webView
+    //        );
+        const gchar *script = "JSON.stringify(window.performance.timing)";
+        webkit_web_view_run_javascript(webView, script, NULL, web_view_javascript_finished, NULL);
 
 }
 static void destroyWindowCb(GtkWidget* widget, GtkWidget* window)
@@ -160,16 +206,34 @@ int main(int argn, char **argv)
 	webView = WEBKIT_WEB_VIEW(
             webkit_web_view_new_with_user_content_manager(webkit_user_content_manager_new()));
     //webView =WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(context));
-
-    setting = webkit_settings_new();
+    setting = webkit_web_view_get_settings(webView);
     webkit_settings_set_enable_page_cache(setting, false);
     webkit_settings_set_enable_javascript(setting, true);
     webkit_settings_set_enable_offline_web_application_cache(setting, false);
 	//webView = WEBKIT_WEB_VIEW(webkit_web_view_new_with_settings(setting));
+    g_object_set(setting,
+	//	"enable-private-browsing", NanBooleanOptionValue(opts, H("private"), false),
+		"enable-plugins", FALSE,
+		"print-backgrounds", TRUE,
+		"enable-javascript", TRUE,
+		"enable-html5-database", FALSE,
+		"enable-html5-local-storage", FALSE,
+		"enable-java", FALSE,
+		"enable-page-cache", FALSE,
+		"enable-write-console-messages-to-stdout", TRUE,
+		"enable-offline-web-application-cache", FALSE,
+	//	"auto-load-images", NanBooleanOptionValue(opts, H("images"), true),
+		"zoom-text-only", FALSE,
+		"media-playback-requires-user-gesture", FALSE, // effectively disables media playback ?
+	//	"user-agent", ua,
+        NULL
+	);
 
 	gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webView));
 	g_signal_connect(webView, "ready-to-show", G_CALLBACK(ready_to_show), NULL);
 	g_signal_connect(webView, "load-changed", G_CALLBACK(web_view_load_changed), NULL);
+	g_signal_connect(webView, "web-view-ready", G_CALLBACK(ready_to_show), NULL);
+
     g_signal_connect(webView, "resource-load-started", G_CALLBACK(resource_started), window);
 
     g_signal_connect(window, "destroy", G_CALLBACK(destroyWindowCb), NULL);
